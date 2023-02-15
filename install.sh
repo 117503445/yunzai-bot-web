44ec6cfa2f0295ab9bd144c3a5ca4a3fc89f9fcd
117503445 <t117503445@gmail.com>
2023-02-15 18:48:30 +0800

    build: yunzai-bot install script
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ install.sh
@@ -0,0 +1,10 @@
+git clone --depth=1 -b main https://gitee.com/Le-niao/Yunzai-Bot.git
+rm -rf ./Yunzai-Bot/.git
+git clone --depth=1 https://github.com/yoimiya-kokomi/miao-plugin.git ./Yunzai-Bot/plugins/miao-plugin
+rm -rf ./Yunzai-Bot/plugins/miao-plugin/.git
+
+cd Yunzai-Bot
+pnpm install -P
+pnpm add image-size -w
+
+# node app
