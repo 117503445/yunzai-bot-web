b86ce60396e983244b8cd839d7b6d1504eccd6a8
117503445 <t117503445@gmail.com>
2023-02-21 10:32:24 +0000

    refactor: remove yunzai-web
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- yunzai-web/node-jiti/yunzai-web-nuxt.config.ts.0949534b.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; // https://nuxt.com/docs/api/configuration/nuxt-config
-var _default = defineNuxtConfig({
-
-});exports.default = _default; /* v7-9022f56f547e57da */
