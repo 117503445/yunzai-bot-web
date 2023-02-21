93708ecbde9c5b830f03e376b57bd36bb382b073
117503445 <t117503445@gmail.com>
2023-02-21 23:05:59 +0800

    feat(fe): naive-ui
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- yunzai-web-fe/src/views/AboutView.vue
+++ yunzai-web-fe/src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="about">
+  <!-- <div class="about">
     <h1>This is an about page</h1>
-  </div>
+  </div> -->
 </template>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
--- yunzai-web-fe/src/views/HomeView.vue
+++ yunzai-web-fe/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <n-button>naive-ui</n-button>
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+import { NButton } from 'naive-ui'
+</script>
