8dadf5a00b8b9423fe438956fa0afe7d3cb0a89d
117503445 <t117503445@gmail.com>
2023-11-21 23:35:45 +0800

    feat: docker-entrypoint.py
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ src/Yunzai-Bot/docker-entrypoint.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+
+# copy every plugin in ./user_plugins to ./plugins, like ./user_plugins/miao -> ./plugins/miao
+def cp_plugins():
+    # first check whether plugin has existed in ./plugins
+    for plugin in os.listdir("./user_plugins"):
+        if os.path.exists("./plugins/" + plugin):
+            print("plugin %s has existed in ./plugins" % plugin)
+            exit(1)
+        else:
+            # copy
+            subprocess.call(["cp", "-r", "./user_plugins/" + plugin, "./plugins"])
+            print("copy plugin %s to ./plugins" % plugin)
+            
+
+def run_server():
+    subprocess.call(["node", "./lib/tools/server.js"])
+
+
+def main():
+    cp_plugins()
+    run_server()
+
+
+if __name__ == '__main__':
+    main()
